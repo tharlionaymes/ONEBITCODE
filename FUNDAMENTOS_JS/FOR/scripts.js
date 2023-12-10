2caaa197e5e37959afbc1d39bee1c77f8df4d43a
Tharlion Aymes <91000905+tharlionaymes@users.noreply.github.com>
2023-12-10 15:32:35 -0400

    aprendendo o FOR

---
+++ FUNDAMENTOS_JS/FOR/scripts.js
@@ -0,0 +1,12 @@
+//inicialização; condição; finalização
+
+for(let indice = 0; indice <= 5; indice ++) {
+    alert("Índice = " + indice)
+}
+
+let nome = "Tharlion Aymes"
+
+for (let indice = 0; indice < nome.length; indice ++){
+    alert(nome[indice])
+
+}
