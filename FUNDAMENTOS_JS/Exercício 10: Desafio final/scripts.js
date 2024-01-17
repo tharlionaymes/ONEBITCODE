522f871bf63b5d60a122a2818df27d579feae835
Tharlion Aymes <91000905+tharlionaymes@users.noreply.github.com>
2024-01-17 15:19:54 -0400

    atualizacao do exercicio 9 e começo do 10

---
+++ FUNDAMENTOS_JS/Exercício 10: Desafio final/scripts.js
@@ -0,0 +1,5 @@
+let menu = 0;
+
+while (menu !== 6){
+    menu = parseInt(prompt("Sistema de Vagas de Emprego:\n1 - Vagas Disponíveis\n2 - Criar Nova Vaga\n3 - Visualizar uma Vaga\n4 - Inscrever um candidato em uma vaga\n5 - Excluir uma vaga\n6 - Sair"));
+};
--- FUNDAMENTOS_JS/Exercício 9: Calculadora geométrica/script.js
+++ FUNDAMENTOS_JS/Exercício 9: Calculadora geométrica/script.js
@@ -1,72 +1,94 @@
 function calcularAreaTriangulo() {
-    let base = parseFloat(prompt("Digite a Base do Triângulo: "));
-    let altura = parseFloat(prompt(`A Base é igual a ${base}.\n\nDigite a Altura do Triângulo: `));
+  let base = parseFloat(prompt("Digite a Base do Triângulo: "));
+  let altura = parseFloat(
+    prompt(`A Base é igual a ${base}.\n\nDigite a Altura do Triângulo: `)
+  );
 
-    let area = (base * altura) / 2;
-    
-    alert(`A Base é igual a: ${base}\nA Altura é igual a: ${altura}\nÁrea do Triângulo é (${base} x ${altura}) / 2 = ${area}`);    
-    
-    return area;
-};
+  let area = (base * altura) / 2;
+
+  alert(
+    `A Base é igual a: ${base}\nA Altura é igual a: ${altura}\nÁrea do Triângulo é (${base} x ${altura}) / 2 = ${area}`
+  );
+
+  return area;
+}
 
 function calcularAreaRetangulo() {
-    let base = parseFloat(prompt("Digite a Base do Retângulo: "));
-    let altura = parseFloat(prompt(`A Base é igual a ${base}.\n\nDigite a Altura do Retângulo: `));
+  let base = parseFloat(prompt("Digite a Base do Retângulo: "));
+  let altura = parseFloat(
+    prompt(`A Base é igual a ${base}.\n\nDigite a Altura do Retângulo: `)
+  );
 
-    let area = (base * altura);
-    
-    alert(`A Base é igual a: ${base}\nA Altura é igual a: ${altura}\nÁrea do Retângulo é (${base} x ${altura}) = ${area}`);    
-    
-    return area;
-};
+  let area = base * altura;
+
+  alert(
+    `A Base é igual a: ${base}\nA Altura é igual a: ${altura}\nÁrea do Retângulo é (${base} x ${altura}) = ${area}`
+  );
+
+  return area;
+}
 
 function calcularAreaQuadrado() {
-    let lado = parseFloat(prompt("Digite o Lado do Quadrado: "));
+  let lado = parseFloat(prompt("Digite o Lado do Quadrado: "));
 
-    let area = (lado * lado);
-    
-    alert(`A Lado é igual a: ${lado}\nÁrea do Quadrado é (${lado} x ${lado}) = ${area}`);    
-    
-    return area;
-};
+  let area = lado * lado;
+
+  alert(
+    `A Lado é igual a: ${lado}\nÁrea do Quadrado é (${lado} x ${lado}) = ${area}`
+  );
+
+  return area;
+}
 
 function calcularAreaTrapezio() {
-    let baseMaior = parseFloat(prompt("Digite a Base Maior do Trapézio: "));
-    let baseMenor = parseFloat(prompt(`A Base Maior é igual a ${baseMaior}.\n\nDigite a Base Menor do Trapêzio: `));
-    let altura = parseFloat(prompt(`A Base Maior é igual a ${baseMaior}.\nA Base Menor é igual a ${baseMenor}\n\nDigite a Altura do Trapêzio: `));
+  let baseMaior = parseFloat(prompt("Digite a Base Maior do Trapézio: "));
+  let baseMenor = parseFloat(
+    prompt(
+      `A Base Maior é igual a ${baseMaior}.\n\nDigite a Base Menor do Trapêzio: `
+    )
+  );
+  let altura = parseFloat(
+    prompt(
+      `A Base Maior é igual a ${baseMaior}.\nA Base Menor é igual a ${baseMenor}\n\nDigite a Altura do Trapêzio: `
+    )
+  );
 
-    let area = ((baseMaior + baseMenor) * altura) / 2;
-    
-    alert(`A Base Maior é igual a: ${baseMaior}\n A Base Menor é igual a ${baseMenor}\nA Altura é igual a: ${altura}\nÁrea do Trapêzio é ((${baseMaior} + ${baseMenor}) * ${altura}) / 2 = ${area}`);    
-    
-    return area;
-};
+  let area = ((baseMaior + baseMenor) * altura) / 2;
+
+  alert(
+    `A Base Maior é igual a: ${baseMaior}\n A Base Menor é igual a ${baseMenor}\nA Altura é igual a: ${altura}\nÁrea do Trapêzio é ((${baseMaior} + ${baseMenor}) * ${altura}) / 2 = ${area}`
+  );
+
+  return area;
+}
 
 function calcularAreaCirculo() {
-    let raio = parseFloat(prompt("Digite o Raio do Círculo: "));
+  let raio = parseFloat(prompt("Digite o Raio do Círculo: "));
 
-    let area = (3.14 * (raio * raio));
-    
-    alert(`O Raio é igual a: ${raio}\nÁrea do Círculo é (pi = 3,14 x ${raio}²) = ${area}`);    
-    
-    return area;
-};
+  let area = 3.14 * (raio * raio);
 
-function calcularTodasAreas(){
-    let base = parseFloat(prompt("Digite a Base: "));
-    let altura = parseFloat(prompt("Digite a Altura: "));
-    let lado = parseFloat(prompt("Digite o Lado: "))
-    let baseMaior = parseFloat(prompt("Digite a Base Maior: "));
-    let baseMenor = parseFloat(prompt("Digite a Base Menor: "));
-    let raio = parseFloat(prompt("Digite o Raio: "));
+  alert(
+    `O Raio é igual a: ${raio}\nÁrea do Círculo é (pi = 3,14 x ${raio}²) = ${area}`
+  );
 
-    let area1 = (base * altura) / 2;
-    let area2 = (base * altura);
-    let area3 = (lado * lado);
-    let area4 = ((baseMaior + baseMenor) * altura) / 2;
-    let area5 = (3.14 * (raio * raio));
-    
-    alert(`
+  return area;
+}
+
+function calcularTodasAreas() {
+  let base = parseFloat(prompt("Digite a Base: "));
+  let altura = parseFloat(prompt("Digite a Altura: "));
+  let lado = parseFloat(prompt("Digite o Lado: "));
+  let baseMaior = parseFloat(prompt("Digite a Base Maior: "));
+  let baseMenor = parseFloat(prompt("Digite a Base Menor: "));
+  let raio = parseFloat(prompt("Digite o Raio: "));
+
+  let area1 = (base * altura) / 2;
+  let area2 = base * altura;
+  let area3 = lado * lado;
+  let area4 = ((baseMaior + baseMenor) * altura) / 2;
+  let area5 = 3.14 * (raio * raio);
+
+  alert(`
     A Base = ${base}
     A Altura = ${altura}    
     O Lado = ${lado}
@@ -78,45 +100,49 @@ function calcularTodasAreas(){
     Área do Quadrado é (${lado} x ${lado}) = ${area3}
     Área do Trapêzio é ((${baseMaior} + ${baseMenor}) * ${altura}) / 2 = ${area4}
     Área do Círculo é (pi = 3,14 x ${raio}²) = ${area5}
-    `); 
-};
+    `);
+}
 
-opcao = 0;
+let opcao = 0;
 
-while (opcao!==7){
-    opcao = parseInt(prompt("Escolha uma Opção:\n1 - Área do Triangulo\n2 - Área do Retângulo\n3 - Área do Quadrado\n4 - Área do Trapézio\n5 - Área do Círculo\n6 - Todas as Opções Acima\n7 - Sair"));
-    
-    switch (opcao) {
-        case 1:
-            calcularAreaTriangulo()
-            break;
-    
-        case 2:
-            calcularAreaRetangulo()
-            break;
-    
-        case 3:
-            calcularAreaQuadrado()
-            break;
-    
-        case 4:
-            calcularAreaTrapezio()
-            break;
-    
-        case 5:
-            calcularAreaCirculo()
-            break;
-    
-        case 6:
-            calcularTodasAreas()
-            break;
-    
-        case 7:
-            alert("Saindo....")
-            break;
-    
-        default:
-            alert("Digite um número válido!!!")
-            break;
-    }
+while (opcao !== 7) {
+  opcao = parseInt(
+    prompt(
+      "Escolha uma Opção:\n1 - Área do Triangulo\n2 - Área do Retângulo\n3 - Área do Quadrado\n4 - Área do Trapézio\n5 - Área do Círculo\n6 - Todas as Opções Acima\n7 - Sair"
+    )
+  );
+
+  switch (opcao) {
+    case 1:
+      calcularAreaTriangulo();
+      break;
+
+    case 2:
+      calcularAreaRetangulo();
+      break;
+
+    case 3:
+      calcularAreaQuadrado();
+      break;
+
+    case 4:
+      calcularAreaTrapezio();
+      break;
+
+    case 5:
+      calcularAreaCirculo();
+      break;
+
+    case 6:
+      calcularTodasAreas();
+      break;
+
+    case 7:
+      alert("Saindo....");
+      break;
+
+    default:
+      alert("Digite um número válido!!!");
+      break;
+  }
 }
