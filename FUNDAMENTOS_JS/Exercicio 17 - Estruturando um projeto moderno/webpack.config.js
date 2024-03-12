e0520e45d35622856e1c20e3d1103dda59c9c844
Tharlion Aymes <91000905+tharlionaymes@users.noreply.github.com>
2024-03-11 23:54:09 -0400

    alteração do webpack.config.js

--- FUNDAMENTOS_JS/Exercicio 17 - Estruturando um projeto moderno/webpack.config.js
+++ FUNDAMENTOS_JS/Exercicio 17 - Estruturando um projeto moderno/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path')
-
-module.exports = {
-    entry: {
-        index:"./src/index.js"
+module.exports = { // Exporta a configuração do webpack
+    entry: { // Define os pontos de entrada da aplicação
+        index: "./src/index.js" // O ponto de entrada é o arquivo index.js localizado no diretório src
     },
-    mode:"development",
-    module:{
-        rules:[{
-            test: /\.css$/,
-            use:["style-loader", "css-loader"],
-        }, 
-        
-        {   test: /\.js$/,
-            exclude: /node_modules/,
-            use: ["babel-loader"]
-        }]
+    mode: "development", // Define o modo de construção como desenvolvimento
+    module: { // Define as regras para processar diferentes tipos de arquivos durante a construção
+        rules: [ // Define as regras de processamento dos arquivos
+            {
+                test: /\.css$/, // Aplica essa regra a arquivos CSS
+                use: ["style-loader", "css-loader"] // Utiliza os loaders style-loader e css-loader para processar arquivos CSS
+            }, 
+            { // Segunda regra
+                test: /\.js$/, // Aplica essa regra a arquivos JavaScript
+                exclude: /node_modules/, // Exclui o diretório node_modules do processamento
+                use: ["babel-loader"] // Utiliza o babel-loader para transpilar arquivos JavaScript
+            }
+        ]
     },
-    output:{
-        filename: "[name].min.js"
+    output: { // Define as opções de saída
+        filename: "[name].min.js" // Define o nome do arquivo de saída como [nome].min.js
     }
 }
